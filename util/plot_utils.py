07165e37914bbe395c90d91da94157761d6cec5b
Less Wright <lessw@etrillium.com>
2020-06-12 01:20:44 -0700

    improve robustness of plot_logs function, handle single directory argument for 'logs' param (#62)
    * improve robustness of plot_logs function:
    - robustify logs param - checks if list, if not, deep converts to list for plotting.
    - expose log name as optional param (log_name = 'log.txt') to avoid internal hard-coded references
    - verify directories passed in are valid.  Raise ValueError if not with path to broken directory shown
    - added parameter explanations in commments
    
    * whitespace fixes for pylint
    
    * pylint flake8 update for whitespace
    
    * pylint flake8 update for whitespace, last 4 issues
    
    * code pylint missing 2 trailing wspace, fixed
    
    * blank line at end of file removed...
    
    * simplified use case to path/list[path]
    
    * 2 whitespace fixes
    
    * one last whitespace removed

--- util/plot_utils.py
+++ util/plot_utils.py
@@ -1,16 +1,50 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 """
 Plotting utilities to visualize training logs.
 """
 import torch
 import pandas as pd
-from pathlib import Path
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from pathlib import Path, PurePath
 
-def plot_logs(logs, fields=('class_error', 'loss_bbox_unscaled', 'mAP'), ewm_col=0):
-    dfs = [pd.read_json(Path(p) / 'log.txt', lines=True) for p in logs]
+
+def plot_logs(logs, fields=('class_error', 'loss_bbox_unscaled', 'mAP'), ewm_col=0, log_name='log.txt'):
+    '''
+    Function to plot specific fields from training log(s). Plots both training and test results.
+
+    :: Inputs - logs = list containing Path objects, each pointing to individual dir with a log file
+              - fields = which results to plot from each log file - plots both training and test for each field.
+              - ewm_col = optional, which column to use as the exponential weighted smoothing of the plots
+              - log_name = optional, name of log file if different than default 'log.txt'.
+
+    :: Outputs - matplotlib plots of results in fields, color coded for each log file.
+               - solid lines are training results, dashed lines are test results.
+
+    '''
+    func_name = "plot_utils.py::plot_logs"
+
+    # verify logs is a list of Paths (list[Paths]) or single Pathlib object Path,
+    # convert single Path to list to avoid 'not iterable' error
+
+    if not isinstance(logs, list):
+        if isinstance(logs, PurePath):
+            logs = [logs]
+            print(f"{func_name} info: logs param expects a list argument, converted to list[Path].")
+        else:
+            raise ValueError(f"{func_name} - invalid argument for logs parameter.\n \
+            Expect list[Path] or single Path obj, received {type(logs)}")
+
+    # verify valid dir(s) and that every item in list is Path object
+    for i, dir in enumerate(logs):
+        if not isinstance(dir, PurePath):
+            raise ValueError(f"{func_name} - non-Path object in logs argument of {type(dir)}: \n{dir}")
+        if dir.exists():
+            continue
+        raise ValueError(f"{func_name} - invalid directory in logs argument:\n{dir}")
+
+    # load log file(s) and plot
+    dfs = [pd.read_json(Path(p) / log_name, lines=True) for p in logs]
 
     fig, axs = plt.subplots(ncols=len(fields), figsize=(16, 5))
 
