ff8389755887445dfab74afbbfbcc3949313d6f1
Wok <woctezuma@users.noreply.github.com>
2020-07-27 17:23:24 +0200

    Set non-strict weight loading
    https://gist.github.com/mlk1337/651297e28199b4bb7907fc413c49f58f

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ def main(args):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
-        model_without_ddp.load_state_dict(checkpoint['model'])
+        model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
             lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
