f4a4550b1f0bd3c15e27290977e57e0acd210609
Nickleha <67520178+Nickleha@users.noreply.github.com>
2020-11-02 14:47:46 -0600

    reorder object class functions. More robust rotation apply to fix random placement.

--- Blender/blender_automation/src/BlenderAPI/BlenderObject.py
+++ Blender/blender_automation/src/BlenderAPI/BlenderObject.py
@@ -169,46 +169,6 @@ class BlenderObject(object):
         '''
         return self.reference.location
 
-    def place_randomly(self, params):
-        """
-        Chooses random coords for object placement. 
-        Requires dict of object states. 
-        Should be run after resize is done.
-        parameters:
-            params: dict
-                attribute required for an object to be placed randomly, [To be completed]
-        """
-        #rotate around unconstrained axis. Will need more robust logic for more than 1 axis
-        #Have to force scene update to get new x/y dimensions post rotation, want to avoid in loop for performance
-        #Moved rotation such that it's tried once per object then only coordinates are changed
-        rotation = [axis*random.uniform(0, 2*pi) for axis in params['unconstrained_axis']]
-        self.set_euler_rotation(rotation[0], rotation[1], rotation[2])
-        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
-        #Bounding box vertices stored in 8*3 array. Object based coordinates, need to apply scale
-        #1st 3 number array is low_x, low_y, low_z
-        #7th 3 number array is high_x, high_y, high_z
-        bound_box = bpy.data.objects[self.name].bound_box
-        dim_x = (bound_box[6][0] - bound_box[0][0]) * params['scale_factor']
-        dim_y = (bound_box[6][1] - bound_box[0][0]) * params['scale_factor']
-        #hardcoded numbers for shelf width of base fridge model, offset by item width
-        x_lims = [-0.25493 + dim_x/2, 0.29941 - dim_x/2]
-        #hardcoded numbers for shelf width of base fridge model, offset by item width
-        y_lims = [-0.4206 + dim_y/2, 0.025957 - dim_y/2]
-        retry_tracker = True
-        attempts = 0
-        while retry_tracker == True and attempts < 9:
-            print(f'\n{self.name} Dimensions:\n{self.reference.dimensions}')
-            #Generate random X,Y,Z coords
-            x_temp = random.uniform(x_lims[0], x_lims[1])
-            y_temp = random.uniform(y_lims[0], y_lims[1])
-            z_temp = find_z_coord(self.name, origin_center=params['origin']=='CENTER', shelf_num=random.choice(params['shelves']))
-            self.set_location(x=x_temp, y=y_temp, z=z_temp)
-            # calling a class method to check if the object is intersecting with others
-            retry_tracker = self.is_intersecting()
-            attempts +=1
-            if attempts == 9:#place at origin if too many tries, removes from render for this image. 
-                self.set_location(0,0,0)
-
     def set_euler_rotation(self, x, y, z):
         ''' set euler orientation for the object in the scene `set_euler_rotation(x, y, z)`
             parameters:
@@ -229,10 +189,10 @@ class BlenderObject(object):
         q = to_quaternion(w, x, y, z)
         self.reference.rotation_quaternion = q
 
-    def set_scale(self, _scale_):
+    def set_scale(self, scale):
         ''' set scale for the object in the scene `set_scale(_scale_)
             parameters:
-                _scale_: iterable of size 3
+                scale: iterable of size 3
                     set the scale of the object to the (scale[0], scale[1], scale[2])
         '''
         valid = check_is_iter(scale, 3) and check_vector_non_negative(scale)
@@ -247,6 +207,18 @@ class BlenderObject(object):
                 absolute euler rotation of the object / bpy.data.objects['xxx'].rotation_euler
         '''
         return self.reference.rotation_euler
+    
+    def apply_rotation(self, ):
+        '''Force rotation changes to be transferred from object rotation to global position change. 
+            Needed for updated bounding boxes.
+        '''
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.data.objects[self.name].select_set(True)
+        for child in bpy.data.objects[self.name].children:
+            child.select_set(True)
+            for subchild in child.children:
+                subchild.select_set(True)
+        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
 
     def get_rotation(self, ):
         ''' get quaternion orientation parameters
@@ -342,3 +314,42 @@ class BlenderObject(object):
         end = time()
         print(f'[{self.name}] No Intersection Found  Time Elapsed: {(end-start)} seconds')
         return False
+
+    def place_randomly(self, params):
+        """
+        Chooses random coords for object placement. 
+        Requires dict of object states. 
+        Should be run after resize is done.
+        parameters:
+            params: dict
+                attribute required for an object to be placed randomly, [To be completed]
+        """
+        #rotate around unconstrained axis. Will need more robust logic for more than 1 axis
+        #Have to force scene update to get new x/y dimensions post rotation, want to avoid in loop for performance
+        rotation = [axis*random.uniform(0, 2*pi) for axis in params['unconstrained_axis']]
+        self.set_euler_rotation(rotation[0], rotation[1], rotation[2])
+        self.apply_rotation()
+        #Bounding box vertices stored in 8*3 array. Object based coordinates, need to apply scale
+        #1st 3 number array is low_x, low_y, low_z
+        #7th 3 number array is high_x, high_y, high_z
+        bound_box = bpy.data.objects[self.name].bound_box
+        dim_x = (bound_box[6][0] - bound_box[0][0]) * params['scale_factor']
+        dim_y = (bound_box[6][1] - bound_box[0][0]) * params['scale_factor']
+        #hardcoded numbers for shelf width of base fridge model, offset by item width
+        x_lims = [-0.25493 + dim_x/2, 0.29941 - dim_x/2]
+        #hardcoded numbers for shelf width of base fridge model, offset by item width
+        y_lims = [-0.4206 + dim_y/2, 0.025957 - dim_y/2]
+        retry_tracker = True
+        attempts = 0
+        while retry_tracker == True and attempts < 9:
+            print(f'\n{self.name} Dimensions:\n{self.reference.dimensions}')
+            #Generate random X,Y,Z coords
+            x_temp = random.uniform(x_lims[0], x_lims[1])
+            y_temp = random.uniform(y_lims[0], y_lims[1])
+            z_temp = find_z_coord(self.name, origin_center=params['origin']=='CENTER', shelf_num=random.choice(params['shelves']))
+            self.set_location(x=x_temp, y=y_temp, z=z_temp)
+            # calling a class method to check if the object is intersecting with others
+            retry_tracker = self.is_intersecting()
+            attempts +=1
+            if attempts == 9:#place at origin if too many tries, removes from render for this image. 
+                self.set_location(0,0,0)
