0ca2bcda3424f23145d1c05e8a1f78d243edd1c2
Arun Singh <singh.arun@iitkgp.ac.in>
2020-11-05 16:41:04 +0530

    Fix annotation bugs

--- Blender/blender_automation/src/render_poses.py
+++ Blender/blender_automation/src/render_poses.py
@@ -18,7 +18,7 @@ sys.path.append(os.path.dirname(MODULE_DIRECTORY))
 # To import python packages like pandas
 #TODO generalize this path
 try: # bad hack just for the time being so that 
-    we don't have to change it everytime we ran on each of our system
+    #we don't have to change it everytime we ran on each of our system
     sys.path.append('/home/solus/anaconda3/lib/python3.8/site-packages')
 except:
     sys.path.append(r"C:\Users\nickh\anaconda3\Lib\site-packages")
@@ -35,18 +35,19 @@ start = time()
 
 # Open a file for writing object annotations
 write_annotation = open('annotations.csv', 'w')
+write_annotation.write('file_path\tobject_name\ttop_left_xy\tright_bottom_xy\n')
 
 # Creating a RenderInterface which would be doing all the importing and
 # placement of the objects, along with the scene/rendering setup
 RI = RenderInterface(resolution=(400,600), samples=128, set_high_quality=True)
 
 
-for i in range(60):
+for i in range(1):
   RI.scene.delete_all()
   RI.setup_blender(resolution=(400,600), samples=128, set_high_quality=True)
   RI.place_all(repeat_objects=True)
 
-  imgs_per_shuffle = 10
+  imgs_per_shuffle = 1
   for j in range(imgs_per_shuffle):
     single_img_time = time()
     RI.shuffle_objects()
@@ -63,10 +64,11 @@ for i in range(60):
     annotation = RI.scene.get_annotation()
     #TODO could be written directly in COCO format
     for ann in annotation:
-    	left_top = annotation[ann]
-    	right_bottom = annotation[ann]
+        object_name = annotation[ann][0]
+        left_top = annotation[ann][1]
+        right_bottom = annotation[ann][2]
       #TODO import params JSON file and convert 3d model object name to the actual label, Could be part of post annotation processing seperately
-    	write_annotation.write(f'{file_path},{ann},{left_top[0]},{left_top[1]},{right_bottom[0]},{right_bottom[1]}\n')
+        write_annotation.write(f'{file_path}\t{object_name}\t({left_top[0]},{left_top[1]})\t({right_bottom[0]},{right_bottom[1]})\n')
 
 end = time()
 print(f'\n\n\n:: Total time elapsed in rendering and replacements: {end-start}')
