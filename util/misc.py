0880534c93dbceb16d40fad049ed0dcd683a3cd3
Benedikt Franke <benedikt.franke@uni-ulm.de>
2021-04-08 08:44:56 +0200

    Fixed torchvision version check

--- util/misc.py
+++ util/misc.py
@@ -18,7 +18,7 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.7:
+if tuple(map(int, torchvision.__version__.split(".")[:2])) < (0, 7):
     from torchvision.ops import _new_empty_tensor
     from torchvision.ops.misc import _output_size
 
