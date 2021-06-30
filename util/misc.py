eb9f7e03ed8e2ed2cd55528989fe7df890bc3fc0
jeonhobeom <jhb1365@gmail.com>
2021-06-30 19:31:23 +0900

    Update misc.py for torchvision 0.10.0 (#404)
    * Update misc.py
    
    torchvision 0.10.0 surpport
    
    * Update misc.py
    
    * Update misc.py

--- util/misc.py
+++ util/misc.py
@@ -18,7 +18,7 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.7:
+if float(torchvision.__version__.split(".")[1]) < 7.0:
     from torchvision.ops import _new_empty_tensor
     from torchvision.ops.misc import _output_size
 
@@ -454,7 +454,7 @@ def interpolate(input, size=None, scale_factor=None, mode="nearest", align_corne
     This will eventually be supported natively by PyTorch, and this
     class can go away.
     """
-    if float(torchvision.__version__[:3]) < 0.7:
+    if float(torchvision.__version__.split(".")[1]) < 7.0:
         if input.numel() > 0:
             return torch.nn.functional.interpolate(
                 input, size, scale_factor, mode, align_corners
