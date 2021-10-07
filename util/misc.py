93230ae00fa271bfc6a89d867a09abdc407f9466
Ani Karapetyan <ani.karapetyan@uni-bonn.de>
2021-10-07 17:22:26 +0200

    Fixed torchvision version checking.

--- util/misc.py
+++ util/misc.py
@@ -10,6 +10,7 @@ import time
 from collections import defaultdict, deque
 import datetime
 import pickle
+from packaging import version
 from typing import Optional, List
 
 import torch
@@ -18,7 +19,7 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__.split(".")[1]) < 7.0:
+if version.parse(torchvision.__version__) < version.parse('0.7'):
     from torchvision.ops import _new_empty_tensor
     from torchvision.ops.misc import _output_size
 
@@ -454,7 +455,7 @@ def interpolate(input, size=None, scale_factor=None, mode="nearest", align_corne
     This will eventually be supported natively by PyTorch, and this
     class can go away.
     """
-    if float(torchvision.__version__.split(".")[1]) < 7.0:
+    if version.parse(torchvision.__version__) < version.parse('0.7'):
         if input.numel() > 0:
             return torch.nn.functional.interpolate(
                 input, size, scale_factor, mode, align_corners
