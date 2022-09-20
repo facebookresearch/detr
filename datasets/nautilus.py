ddcf06e03d1ef151bfaf632ca76be9aa0d1a492e
aravi-miovision <aravi@miovision.com>
2022-09-20 14:09:01 -0400

    added a buffer functionality to nautilus

--- datasets/nautilus.py
+++ datasets/nautilus.py
@@ -11,25 +11,31 @@ import tf_utils
 import torchvision.transforms as T
 # import datasets.transforms as T
 
+from tqdm import tqdm
+
 class NautilusDataset(torch.utils.data.Dataset):
     def __init__(
         self,
         tfrecord_path: str,
         transforms: typing.Optional[typing.Callable] = None,
         is_train: bool = True,
-        input_size: tuple = (300, 300)
+        input_size: tuple = (300, 300),
+        buffer_size: int = 512
     ):
         super(NautilusDataset, self).__init__()
         self._tfrecord_path = tfrecord_path
         self._transforms = transforms
         self._is_train = is_train
         self._input_size = input_size
+        self._buffer_size = buffer_size
 
         if self._transforms is None:
             self._transforms = self.get_default_transforms()
 
         self._dataset = tf.data.TFRecordDataset(self._tfrecord_path)
         self._dataset_iter = iter(self._dataset)
+
+        self.get_new_buffer()
 
     def get_default_transforms(self):
         normalize = T.Compose([
@@ -43,8 +49,22 @@ class NautilusDataset(torch.utils.data.Dataset):
                 T.Resize((self._input_size[0], self._input_size[1])),
                 normalize,
             ])
-
         return normalize
+
+    def get_new_buffer(self):
+        self._read_count = 0
+        self._buffer = list()
+        for i in tqdm(range(self._buffer_size)):
+            try:
+                record = next(self._dataset_iter)
+            except:
+                self._dataset_iter = iter(self._dataset)
+                record = next(self._dataset_iter)
+            record_data = tf_utils.parse_record(record)
+            img = self.get_image(record_data)
+            img = self._transforms(img)
+            target = self.get_targets(record_data)
+            self._buffer.append((img, target))
 
     def get_image(self, record_data):
         return Image.fromarray(record_data.image)
@@ -66,15 +86,13 @@ class NautilusDataset(torch.utils.data.Dataset):
         return target
 
     def __len__(self):
-        pass
+        return 900000
 
     def __getitem__(self, idx):
-        record = next(self._dataset_iter)
-        record_data = tf_utils.parse_record(record)
-        img = self.get_image(record_data)
-        img = self._transforms(img)
-        target = self.get_targets(record_data)
-        return img, target
+        self._read_count += 1
+        if self._read_count >= self._buffer_size:
+            self.get_new_buffer()
+        return self._buffer[(idx%self._buffer_size)]
 
 def build(image_set, args):
     root = Path(args.coco_path)
@@ -85,3 +103,8 @@ def build(image_set, args):
     }
     dataset = NautilusDataset(tfrecord_path=PATHS[image_set])
     return dataset
+
+if __name__=='__main__':
+    nd = NautilusDataset(tfrecord_path="/mnt/storage/2021-09-24-its/train.tfrecord")
+    out = nd[0]
+    breakpoint()
