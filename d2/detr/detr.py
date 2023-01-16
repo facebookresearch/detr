da0a8d5ebaad8e958bb2d6642744bf6fc9683f9d
Aman Sharma <46420566+amancodeblast@users.noreply.github.com>
2023-01-15 23:20:03 -0500

    Changed the code for multiple channel input image compatibility

--- d2/detr/detr.py
+++ d2/detr/detr.py
@@ -150,8 +150,8 @@ class Detr(nn.Module):
         )
         self.criterion.to(self.device)
 
-        pixel_mean = torch.Tensor(cfg.MODEL.PIXEL_MEAN).to(self.device).view(3, 1, 1)
-        pixel_std = torch.Tensor(cfg.MODEL.PIXEL_STD).to(self.device).view(3, 1, 1)
+        pixel_mean = torch.Tensor(cfg.MODEL.PIXEL_MEAN).to(self.device).view(len(cfg.MODEL.PIXEL_MEAN), 1, 1)
+        pixel_std = torch.Tensor(cfg.MODEL.PIXEL_STD).to(self.device).view(len(cfg.MODEL.PIXEL_STD), 1, 1)
         self.normalizer = lambda x: (x - pixel_mean) / pixel_std
         self.to(self.device)
 
