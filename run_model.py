229616d53d55b67218034e3e42f1be0621b14435
nhphucqt <hoangphuc.qti@gmail.com>
2023-12-01 15:49:14 +0700

    test checkponit

--- run_model.py
+++ run_model.py
@@ -90,6 +90,14 @@ def plot_results(pil_img, prob, boxes):
     
 
 if __name__ == "__main__":
+    # COCO classes
+    CLASSES = [
+       'person'
+    ]
+
+    # colors for visualization
+    COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+            [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
     # standard PyTorch mean-std input image normalization
     transform = T.Compose([
         T.Resize(800),
