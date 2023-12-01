051c9f62f19bbfc284e5d4e2163687ac1fff8f60
nhphucqt <hoangphuc.qti@gmail.com>
2023-12-01 15:44:58 +0700

    test checkponit

---
+++ run_model.py
@@ -0,0 +1,107 @@
+import torch
+
+from PIL import Image
+import requests
+import matplotlib.pyplot as plt
+import torchvision.transforms as T
+
+
+from models.backbone import Backbone, Joiner
+from models.detr import DETR, PostProcess
+from models.position_encoding import PositionEmbeddingSine
+from models.segmentation import DETRsegm, PostProcessPanoptic
+from models.transformer import Transformer
+
+def _make_detr(backbone_name: str, dilation=False, num_classes=1, mask=False):
+    hidden_dim = 256
+    backbone = Backbone(backbone_name, train_backbone=True, return_interm_layers=mask, dilation=dilation)
+    pos_enc = PositionEmbeddingSine(hidden_dim // 2, normalize=True)
+    backbone_with_pos_enc = Joiner(backbone, pos_enc)
+    backbone_with_pos_enc.num_channels = backbone.num_channels
+    transformer = Transformer(d_model=hidden_dim, return_intermediate_dec=True)
+    detr = DETR(backbone_with_pos_enc, transformer, num_classes=num_classes, num_queries=100)
+    if mask:
+        return DETRsegm(detr)
+    return detr
+
+
+def detr_resnet50(pretrained=False, num_classes=1, return_postprocessor=False):
+    """
+    DETR R50 with 6 encoder and 6 decoder layers.
+
+    Achieves 42/62.4 AP/AP50 on COCO val5k.
+    """
+    model = _make_detr("resnet50", dilation=False, num_classes=num_classes)
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://huggingface.co/nhphucqt/detr_epoch_001.pth/resolve/main/checkpoint.pth", map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    if return_postprocessor:
+        return model, PostProcess()
+    return model
+
+# for output bounding box post-processing
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+def detect(im, model, transform):
+    # mean-std normalize the input image (batch-size: 1)
+    img = transform(im).unsqueeze(0)
+
+    # demo model only support by default images with aspect ratio between 0.5 and 2
+    # if you want to use images with an aspect ratio outside this range
+    # rescale your image so that the maximum size is at most 1333 for best results
+    assert img.shape[-2] <= 1600 and img.shape[-1] <= 1600, 'demo model only supports images up to 1600 pixels on each side'
+
+    # propagate through the model
+    outputs = model(img)
+
+    # keep only predictions with 0.7+ confidence
+    probas = outputs['pred_logits'].softmax(-1)[0, :, :-1]
+    keep = probas.max(-1).values > 0.7
+
+    # convert boxes from [0; 1] to image scales
+    bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep], im.size)
+    return probas[keep], bboxes_scaled
+
+def plot_results(pil_img, prob, boxes):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), COLORS * 100):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        cl = p.argmax()
+        text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.show()
+    
+
+if __name__ == "__main__":
+    # standard PyTorch mean-std input image normalization
+    transform = T.Compose([
+        T.Resize(800),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    detr = detr_resnet50(pretrained=True, num_classes=1, return_postprocessor=False).eval()
+
+    url = 'http://images.cocodataset.org/val2017/000000004134.jpg'
+    im = Image.open(requests.get(url, stream=True).raw)
+
+    scores, boxes = detect(im, detr, transform)
+
+    plot_results(im, scores, boxes)
