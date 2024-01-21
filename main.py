2c51705029c3fd4078a93e151b85490c1a075f3d
Szymon Dziedzic <szymon.dziedzic@swmansion.com>
2024-01-21 12:21:04 +0100

    fix the bug

--- main.py
+++ main.py
@@ -220,7 +220,7 @@ def main(args):
                     }, checkpoint_path)
                     if wandb_run is not None:
                         print(f"Saving checkpoint to {checkpoint_path}")
-                        wandb_run.save(checkpoint_path)
+                        wandb_run.save(str(checkpoint_path))
 
             test_stats, coco_evaluator = evaluate(
                 model, criterion, postprocessors, data_loader_val, base_ds, device, args.output_dir
